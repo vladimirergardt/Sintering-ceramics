d6341665376023bafb19cd5589d1e25ce456fe82
v.ergardt <v.ergardt@chtp.net>
2018-10-24 18:12:17 +0300

    add db conection

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,4 +1,9 @@
 module.exports = {
-  port: 8081
+  port: 8081,
+  dbURL: `mongodb://vladimir:12345a@ds231643.mlab.com:31643/sintering`,
+  dbOptions: {
+    // useMongoClient: true,
+    useNewUrlParser: true
+  },
 };
 
