4750094b4d308a8286e8ce7838280fd55409cdce
vladimiregardt <vladimiregardt@gmail.com>
2018-11-02 00:01:30 +0300

    fix mongoose connection

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -2,7 +2,6 @@ module.exports = {
   port: 8081,
   dbURL: `mongodb://vladimir:12345a@ds231643.mlab.com:31643/sintering`,
   dbOptions: {
-    // useMongoClient: true,
     useNewUrlParser: true
   },
 };
--- server/src/index.js
+++ server/src/index.js
@@ -4,25 +4,25 @@ const  cors = require('cors')
 const morgan = require('morgan')
 const config = require('./config/config')
 
-// add mongoose
-const mongoose = require('mongoose')
-mongoose.Promise = global.Promise
-
-mongoose.connect(`mongodb://vladimir:12345a@ds231643.mlab.com:31643/sintering`, { useNewUrlParser: true })
-
-mongoose.connection.once('open', () => {
-  console.log('connected to database');
-})
-
-
 const app = express()
 
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.listen(process.env.PORT || config.port,
-  () => console.log(`Server start on port ${ config.port } ...`))
+// add mongoose
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+
+mongoose.connect(config.dbURL, config.dbOptions)
+
+mongoose.connection
+    .once('open', () => {
+        console.log('Mongoose - successful connection...')
+    app.listen(process.env.PORT || config.port,
+      () => console.log(`Server start on port ${config.port}...`))
+  })
+   .on('error', error => console.warn(error))
 
 // first path
 app.get('/posts', (req, res) => {
@@ -34,13 +34,5 @@ app.get('/posts', (req, res) => {
   )
 })
 
-// mongoose.connect(config.dbURL, config.dbOptions)
-//
-// mongoose.connection
-//   .once('open', () => {
-//     console.log('Mongoose - successful connection ...')
-//     app.listen(process.env.PORT || config.port,
-//       () => console.log(`Server start on port ${config.port} ...`))
-//   })
-//   .on('error', error => console.warn(error))
+
 
